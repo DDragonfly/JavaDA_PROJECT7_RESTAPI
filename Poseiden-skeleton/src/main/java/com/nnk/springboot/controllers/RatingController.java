28a29fc8ec83b94dd6a3a34d93ccf636a77deea2
Desiree <desiree.ttt@gmail.com>
2025-12-08 21:05:57 +0100

    implementation rating (get/post), templates, tests

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,6 +1,7 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.services.RatingService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,43 +13,63 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.validation.Valid;
 
 @Controller
+@RequestMapping("/rating")
 public class RatingController {
-    // TODO: Inject Rating service
 
-    @RequestMapping("/rating/list")
-    public String home(Model model)
-    {
-        // TODO: find all Rating, add to model
+    private final RatingService ratingService;
+
+    // Inject Rating service
+    public RatingController(RatingService ratingService) {
+        this.ratingService = ratingService;
+    }
+
+    @RequestMapping("/list")
+    public String home(Model model) {
+        // find all Rating, add to model
+        model.addAttribute("ratings", ratingService.findAll());
         return "rating/list";
     }
 
-    @GetMapping("/rating/add")
+    @GetMapping("/add")
     public String addRatingForm(Rating rating) {
         return "rating/add";
     }
 
-    @PostMapping("/rating/validate")
+    @PostMapping("/validate")
     public String validate(@Valid Rating rating, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Rating list
-        return "rating/add";
-    }
-
-    @GetMapping("/rating/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Rating by Id and to model then show to the form
-        return "rating/update";
-    }
-
-    @PostMapping("/rating/update/{id}")
-    public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Rating and return Rating list
+        // check data valid and save to db, after saving return Rating list
+        if (result.hasErrors()) {
+            return "rating/add";
+        }
+        ratingService.save(rating);
         return "redirect:/rating/list";
     }
 
-    @GetMapping("/rating/delete/{id}")
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
+        // get Rating by Id and to model then show to the form
+        Rating rating = ratingService.findById(id);
+        model.addAttribute("rating", rating);
+        return "rating/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
+                               BindingResult result, Model model) {
+        // check required fields, if valid call service to update Rating and return Rating list
+        if (result.hasErrors()) {
+            rating.setId(id);
+            return "rating/update";
+        }
+        rating.setId(id);
+        ratingService.save(rating);
+        return "redirect:/rating/list";
+    }
+
+    @GetMapping("/delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Rating by Id and delete the Rating, return to Rating list
+        // Find Rating by Id and delete the Rating, return to Rating list
+        ratingService.deleteById(id);
         return "redirect:/rating/list";
     }
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -3,24 +3,91 @@ package com.nnk.springboot.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.sql.Timestamp;
+import jakarta.validation.constraints.Positive;
 
 @Entity
-@Table(name = "rating")
+@Table(name = "Rating")
 public class Rating {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer ratingId;
+    @Column(name = "Id")
+    private Integer id;
+
+    @NotBlank(message = "Moodys rating is mandatory")
+    @Column(name = "moodysRating")
+    private String moodysRating;
+
+    @NotBlank(message = "SandP rating is mandatory")
+    @Column(name = "sandPRating")
+    private String sandPRating;
+
+    @NotBlank(message = "Fitch rating is mandatory")
+    @Column(name = "fitchRating")
+    private String fitchRating;
+
+    @NotNull(message = "Order is mandatory")
+    @Positive(message = "Order must be positive")
+    @Column(name = "orderNumber")
+    private Integer orderNumber;
 
     public Rating() {}
 
-    public Integer getRatingId() {
-        return ratingId;
+    public Rating(String moodysRating, String sandPRating, String fitchRating, Integer orderNumber) {
+        this.moodysRating = moodysRating;
+        this.sandPRating = sandPRating;
+        this.fitchRating = fitchRating;
+        this.orderNumber = orderNumber;
     }
 
-    public void setRatingId(Integer ratingId) {
-        this.ratingId = ratingId;
+    public Integer getId() {
+        return id;
     }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMoodysRating() {
+        return moodysRating;
+    }
+
+    public void setMoodysRating(String moodysRating) {
+        this.moodysRating = moodysRating;
+    }
+
+    public String getSandPRating() {
+        return sandPRating;
+    }
+
+    public void setSandPRating(String sandPRating) {
+        this.sandPRating = sandPRating;
+    }
+
+    public String getFitchRating() {
+        return fitchRating;
+    }
+
+    public void setFitchRating(String fitchRating) {
+        this.fitchRating = fitchRating;
+    }
+
+    public Integer getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(Integer orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    @Transient
+    public Integer getOrder() {
+        return orderNumber;
+    }
+
+    public void setOrder(Integer order) {
+        this.orderNumber = order;
+    }
+
     // TODO: Map columns in data table RATING with corresponding java fields
 }
