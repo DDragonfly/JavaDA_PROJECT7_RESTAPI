3de56346ce45bfa8d17fc6985835cb8120964353
Desiree <desiree.ttt@gmail.com>
2025-12-12 00:55:39 +0100

    feat: implement Trade CRUD templates and tests

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
@@ -2,6 +2,9 @@ package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.sql.Timestamp;
 
 
@@ -11,9 +14,32 @@ public class Trade {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TradeId")
     private Integer tradeId;
 
+    @NotBlank(message = "Account is mandatory")
+    @Column(name = "account")
+    private String account;
+
+    @NotBlank(message = "Type is mandatory")
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "buyQuantity")
+    private Double buyQuantity;
+
     public Trade() {}
+
+    public Trade(String account, String type, Double buyQuantity) {
+        this.account = account;
+        this.type = type;
+        this.buyQuantity = buyQuantity;
+    }
+
+    public Trade(String account, String type) {
+        this.account = account;
+        this.type = type;
+    }
 
     public Integer getTradeId() {
         return tradeId;
@@ -23,5 +49,36 @@ public class Trade {
         this.tradeId = tradeId;
     }
 
-    // TODO: Map columns in data table TRADE with corresponding java fields
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getBuyQuantity() {
+        return buyQuantity;
+    }
+
+    public void setBuyQuantity(Double buyQuantity) {
+        this.buyQuantity = buyQuantity;
+    }
+
+    @Transient
+    public Integer getId() {
+        return tradeId;
+    }
+
+    public void setId(Integer tradeId) {
+        this.tradeId = tradeId;
+    }
 }
